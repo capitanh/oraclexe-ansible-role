efe9fcb426a0135453c89f609f65ac9b86c6f66a
Luis Novo <luis.novo@gmail.com>
2020-07-23 16:09:28 +0200

    Adapt to python3

--- library/oracle_profile.py
+++ library/oracle_profile.py
@@ -123,15 +123,6 @@ else:
 
 # cmp def for python 3
 def cmp(x, y):
-    """
-    Replacement for built-in function cmp that was removed in Python 3
-
-    Compare the two objects x and y and return an integer according to
-    the outcome. The return value is negative if x < y, zero if x == y
-    and strictly positive if x > y.
-    """
-
-    #return (x > y) - (x < y)
     return operator.gt(x,y) - operator.lt(x,y)
 
 # Check if the profile exists
